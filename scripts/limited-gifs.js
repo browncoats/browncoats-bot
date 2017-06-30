4eba41b2ecf65ef8ae8e60e135fa3ec4acd8f587
browncoats <parkwaydrive.serenity@gmail.com>
2017-06-29 19:04:30 -0500

    prettify

--- scripts/limited-gifs.js
+++ scripts/limited-gifs.js
@@ -1,38 +1,38 @@
 module.exports = function(robot) {
 
     robot.userGifUsage = {};
-    var gifChannel = 'jiffs'
-    var gifLimit = 10
-    var gifRating = 'PG-13'
+    var gifChannel = 'jiffs';
+    var gifLimit = 10;
+    var gifRating = 'PG-13';
     
     robot.respond(/gif (.*)/i, function(res) {
         var term = res.match[1];
-        
+
         if (term.length === 0) {
             res.reply('You have to enter a search term if you want to see a gif.');
-    		}
+        }
 
         if(!robot.userGifUsage[res.message.user]) {
-          robot.userGifUsage[res.message.user] = 0;
-			  }
-      
+            robot.userGifUsage[res.message.user] = 0;
+        }
+
         if (res.message.room != gifChannel) {
             if (robot.userGifUsage[res.message.user] >= gifLimit) {
                 res.reply('You\'ve reached your gif limit for the day. Try #' + gifChannel);
-			      }
+            }
             else {
-                res.reply(robot.userGifUsage[res.message.user] + ' of ' + gifLimit + ' gifs used for the day.');               
+                res.reply(robot.userGifUsage[res.message.user] + ' of ' + gifLimit + ' gifs used for the day.');
                 respondWithGif(term, res, true);
             }
-			  }
+        }
         else {
           respondWithGif(term, res, false);
         }
-	  });
-    
+    });
+
     function respondWithGif(searchTerm, msg, increment) {
         var baseUrl = 'https://api.giphy.com/v1/gifs/search';
-        
+
         var data = {
             api_key: process.env.HUBOT_GIPHY_TOKEN,
             q: searchTerm,
@@ -41,28 +41,28 @@ module.exports = function(robot) {
             rating: gifRating,
             lang: 'en'
         };
-		    
+
         var requestUrl = baseUrl;
         var firstTime = true;
         for (var item in data) {
-          if (!!firstTime) {
-            requestUrl += '?';
-            firstTime = false;
-          }
-          else {
-            requestUrl += '&';
-          }
+            if (!!firstTime) {
+                requestUrl += '?';
+                firstTime = false;
+            }
+            else {
+                requestUrl += '&';
+            }
 
-          requestUrl += item + '=' + data[item];
+            requestUrl += item + '=' + data[item];
         }
-        
+
         robot.logger.debug('sending request:' + requestUrl);
         robot.http(requestUrl)
             .header('Accept', 'application/json')
             .get()(function(err, resp, body) {
                 if (err) {
                     robot.reply('Sorry, I couldn\'t find a gif for that search term.');
-				        }
+                }
                 else {
                     robot.logger.debug('response: ' + resp);
                     robot.logger.debug('body: ' + body);
@@ -73,6 +73,6 @@ module.exports = function(robot) {
                       robot.userGifUsage[msg.message.user]++;
                     }
                 }
-			      });
-	  }
+            });
+    }
 }
